424e6e8f959377236f24f902e884b95e32f2d812
André Secco <96576551+AndreSecco@users.noreply.github.com>
2022-04-01 09:27:46 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-
-  transpileDependencies: true,
-}),
 module.exports = {
-  publicPath: '/dashboard/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/dashboard/'
+    : '/'
 }
-
-
-
-  transpileDependencies: true
-})
-module.exports = {
-  publicPath: '/dashboard/'
-}
-
