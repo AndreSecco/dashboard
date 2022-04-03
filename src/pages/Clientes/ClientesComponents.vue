33848cedeeaab7961ae2c4804b1da90deaca535f
Andre <andrefigueiro5@gmail.com>
2022-04-03 19:02:27 -0300

    new file:   css/24.9e332535.css 	new file:   css/255.6076f2fb.css 	new file:   css/260.7d6fb817.css 	new file:   css/298.4f6f35e1.css 	new file:   css/38.9e332535.css 	new file:   css/441.98ac1b91.css 	new file:   css/703.0d843e0e.css 	new file:   css/733.8edcb217.css 	new file:   css/818.493819d7.css 	new file:   css/875.6076f2fb.css 	new file:   css/886.0d843e0e.css 	new file:   css/app.05c2b694.css 	new file:   css/chunk-vendors.e780c236.css 	new file:   favicon-old copy 2.ico 	new file:   fonts/fa-brands-400.edf40f86.woff2 	new file:   fonts/fa-brands-400.f7223235.ttf 	new file:   fonts/fa-regular-400.3665ebc7.woff2 	new file:   fonts/fa-regular-400.a7fde52b.ttf 	new file:   fonts/fa-solid-900.0d2abd43.woff2 	new file:   fonts/fa-solid-900.5b03221c.ttf 	new file:   fonts/fa-v4compatibility.42932bea.ttf 	new file:   img/icone-bootstrap.6ac32336.png 	new file:   img/icone-html.36259293.png 	new file:   img/icone-javascript.95b06606.png 	new file:   img/icone-json.8aa3806c.png 	new file:   img/icone-sass.20e3becb.png 	new file:   img/icone-vue.d812e559.png 	new file:   index copy 2.html 	new file:   index copy.html 	new file:   index.html 	new file:   js/24-legacy.6dd930e7.js 	new file:   js/24-legacy.6dd930e7.js.map 	new file:   js/255-legacy.62780d4a.js 	new file:   js/255-legacy.62780d4a.js.map 	new file:   js/260-legacy.7aac245f.js 	new file:   js/260-legacy.7aac245f.js.map 	new file:   js/260.d661d8f2.js 	new file:   js/260.d661d8f2.js.map 	new file:   js/298-legacy.840fa6a9.js 	new file:   js/298-legacy.840fa6a9.js.map 	new file:   js/298.9275bcdf.js 	new file:   js/298.9275bcdf.js.map 	new file:   js/38.25d13a40.js 	new file:   js/38.25d13a40.js.map 	new file:   js/435-legacy.2cdcb15b.js 	new file:   js/435-legacy.2cdcb15b.js.map 	new file:   js/441-legacy.20218c30.js 	new file:   js/441-legacy.20218c30.js.map 	new file:   js/441.a68ed1b5.js 	new file:   js/441.a68ed1b5.js.map 	new file:   js/703-legacy.c65c4a5d.js 	new file:   js/703-legacy.c65c4a5d.js.map 	new file:   js/733-legacy.eb1bf19e.js 	new file:   js/733-legacy.eb1bf19e.js.map 	new file:   js/733.eb816777.js 	new file:   js/733.eb816777.js.map 	new file:   js/818-legacy.7a80f0f5.js 	new file:   js/818-legacy.7a80f0f5.js.map 	new file:   js/818.ef69f094.js 	new file:   js/818.ef69f094.js.map 	new file:   js/875.3716e496.js 	new file:   js/875.3716e496.js.map 	new file:   js/886.0ffa11be.js 	new file:   js/886.0ffa11be.js.map 	new file:   js/app-legacy.027a06f7.js 	new file:   js/app-legacy.027a06f7.js.map 	new file:   js/app.2f2c144e.js 	new file:   js/app.2f2c144e.js.map 	new file:   js/chunk-vendors-legacy.0c688622.js 	new file:   js/chunk-vendors-legacy.0c688622.js.map 	new file:   js/chunk-vendors.7450072b.js 	new file:   js/chunk-vendors.7450072b.js.map

--- src/pages/Clientes/ClientesComponents.vue
+++ src/pages/Clientes/ClientesComponents.vue
@@ -4,6 +4,139 @@
       <div class="main" slot="slot-pages">
         <div class="pages">
            <h1>Clientes</h1>
+            <Message :msg="msg" v-show="msg" />
+            <div>
+            <b-collapse id="collapse-2" v-model="visible" class="mt-2">
+              <b-form id="produto-form" @submit="insereProduto">
+                <div class="d-flex row">
+                 
+                  <b-form-group
+                    class="col-12 col-sm-6"
+                    id="input-nomeProd"
+                    label="Nome:"
+                    label-for="input-1"
+                  >
+                    <b-form-input
+                      class="col-12"
+                      id="nomeProd"
+                      v-model="nomeProd"
+                      type="text"
+                      placeholder="Nome"
+                      required
+                    >{{nomeProd}}</b-form-input>
+                  </b-form-group>
+
+                  <b-form-group
+                    class="col-12 col-sm-2"
+                    id="input-precoProd"
+                    label="Preço de Venda:"
+                    label-for="input-2"
+                  >
+                    <b-form-input
+                      id="precoProd"
+                      v-model="precoProd"
+                      placeholder="R$"
+                      required
+                    ></b-form-input>
+                  </b-form-group>
+                  <b-form-group
+                    class="col-2"
+                    id="input-group-3"
+                    label="Estoque:"
+                    label-for="input-3"
+                  >
+                    <b-form-input
+                      id="input-estoque"
+                      v-model="estoqueProd"
+                      type="Estoque"
+                      placeholder="Estoque"
+                      required
+                    ></b-form-input>
+                  </b-form-group>
+                </div>
+                <div class="d-flex row">
+                  <b-form-group
+                    class="col-6"
+                    id="input-group-6"
+                    label="Cód. de Barras:"
+                    label-for="input-3"
+                  >
+                    <b-form-input
+                      id="input-descricao"
+                      v-model="codBarras"
+                      type="Cód. de Barras"
+                      placeholder="Cód. de Barras"
+                      required
+                    ></b-form-input>
+                  </b-form-group>
+                </div>
+                <div id="atlista">
+                  <b-button 
+                                
+                    type="submit"
+                    class="mr-1"
+                    variant="success"
+                    @click="(visible = !visible), insereProduto"
+                    >Inserir</b-button
+                  >
+                  <b-button
+                 
+                    class="mr-1"
+                    variant="primary"
+                    @click="(visible = !visible), confirmaUpdate(idProd)"
+                    >Alterar</b-button
+                  >
+                  <b-button
+                    type="reset"
+                    class="mr-1"
+                    @click="(visible = !visible), limpaCampos()"
+                    >Cancelar</b-button
+                  >
+                </div>
+              </b-form>
+            </b-collapse>
+          </div>
+          <div class="mt-3">
+            <!-- Listagem -->
+            <b-button
+              variant="outline-primary"
+              class="mb-3"
+              v-b-toggle.collapse-2
+            >
+              Adicionar Novo Cliente</b-button
+            >
+
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Nome</th>
+                  <th scope="col">Idade</th>
+                  <th scope="col">E-mail</th>
+                  <th scope="col">Ações</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr v-for="cliente in clientes" :key="cliente.id">
+                  <th scope="row">{{ cliente.id }}</th>
+                  <td>{{ cliente.nomeCliente }}</td>
+                  <td>{{ cliente.idadeCliente }}</td>
+                  <td>{{ cliente.emailCliente }}</td>
+                  <td>
+                    <b-button @click="updateProd(produto.id), (visible = !visible)" class="mr-2" variant="outline-primary"
+                      >Editar</b-button
+                    >
+                    <b-button
+                      @click="confirmaExclusao(produto.id)"
+                      variant="outline-danger"
+                      >Excluir</b-button
+                    >
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </DashBoardComponent>
@@ -12,11 +145,40 @@
 
 <script>
 import DashBoardComponent from "../Dashboard/DashBoardComponent.vue";
+import Message from "../../components/Message.vue";
 
 export default {
   name: "ProdutosComponents",
   components: {
-    DashBoardComponent
+    DashBoardComponent,
+    Message
+  },
+  data(){
+    return{
+      clientes: null,
+      nomeCliente: null,
+      idadeCliente: null,
+      emailCliente: null,
+      form: {},
+      show: true,
+      msg: null
+    }
+  },
+  methods:{
+    // LISTA OS CLIENTES
+    async getClientes() {
+      const req = await fetch("http://localhost:3000/clientes");
+      const data = await req.json();
+
+      this.clientes = data;
+
+      console.log(this.clientes);
+    },
+
+
+  },
+  mounted(){
+    this.getClientes()
   },
 };
 </script>
